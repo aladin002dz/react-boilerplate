b0a01c1c1b42f02263e7bd1c66898f75b74dbd16
aladin002dz <mahfoudh.arous@gmail.com>
2023-02-12 20:52:07 +0100

    installing @babel/preset-react

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-import getGreeting from "./greeting";
-console.log(getGreeting("world"));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
