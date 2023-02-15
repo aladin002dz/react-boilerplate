d0e8044b514d7b309d1c2dc1e92dabe2175f52d1
aladin002dz <mahfoudh.arous@gmail.com>
2023-02-15 16:08:28 +0100

    adding dev and production webpack configuration files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 export default function App() {
+    const [counter, setCounter] = useState(0);
     return (
-        <h1>App Updated</h1>
+        <div>
+            <h3>App Updated 3</h3>
+            <h1>{counter}</h1>
+            <div>
+                <button onClick={() => setCounter(counter - 1)}>-</button>
+                <button onClick={() => setCounter(counter + 1)}>+</button>
+            </div>
+
+        </div>
     )
 }
