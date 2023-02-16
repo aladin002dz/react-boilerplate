a893da0e019ceef67a4fb19acb0bac602063e846
aladin002dz <mahfoudh.arous@gmail.com>
2023-02-16 11:06:39 +0100

    adding css support

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -16,10 +16,15 @@ module.exports = {
                 options: {
                     presets: ['@babel/preset-env', '@babel/preset-react']
                 }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+                exclude: /node_modules/
             }
         ]
     },
     plugins: [new HtmlWebpackPlugin({
         template: './src/index.html'
-    })]
+    })],
 }
